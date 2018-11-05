00788e6094a6d42c22c571f2c142edd94132d949
Prahlad <anand39@purdue.edu>
2018-11-05 15:41:06 -0500

    added dataloaders

--- train.py
+++ train.py
@@ -6,13 +6,9 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from pathlib import Path
-import re, string
-import unicodedata
-import inflect
-import nltk
-import os
 from data.build_dataset import build_dataset
 from fastai.text import *
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, help='path to csv file with data')
@@ -22,11 +18,13 @@ args = parser.parse_args()
 
 class ModelTrainer():
     def __init__(self):
+
         datapath = args.data
-        databunch = TextClasDataBunch.from_csv(datapath)
+        self.databunch = TextClasDataBunch.from_csv(datapath)
+        self.train_dataloader = databunch.train_dl
+        self.valid_dataloader = databunch.valid_dl
+
+
 
     def train():
-        pass
-
-if __name__ == "__main__":
-    LSTM = ModelTrainer()
+	pass
