3a98a959c42dd05ff9ee666e3783dd7169d18664
prahladanand17 <anprahlad@gmail.com>
2018-11-10 18:47:39 -0500

    initializing model and embeddings

--- models/LSTM.py
+++ models/LSTM.py
@@ -9,19 +9,27 @@ from fastai.text import *
 
 
 class LSTM(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_size):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, word_to_idx, glove_path):
         super(LSTM,self).__init__()
+        #load pretrained gloVe embeddings
 
-
+        word_embeds = load(glove_path)
+        embeddings = np.zeros((vocab_size, embedding_size))
+        for word in word_embeds.keys():
+            index = word_to_idx[word]
+            embeddings[index] = word_embeds[word]
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_size).cuda()
+        self.word_embeddings.weight.data.copy_(torch.from_numpy(embeddings))
 
 
         self.embeds = nn.Embedding(vocab_size, embedding_dim)
         self.doc_LSTM = nn.LSTM(embedding_dim, hidden_size, 1, batch_first = True)
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_size, )
+            nn.Linear(hidden_size, 50)
             nn.ReLu(),
             nn.Dropout(p=0.5),
-            nn.Linear(,20)
+            nn.Linear(50,20)
         )
     def forward(self, input):
-        pass
+        word_embeddings = self.word_embeddings(input)
+        import pdb; pdb.set_trace()
--- train.py
+++ train.py
@@ -9,9 +9,11 @@ from pathlib import Path
 from data.build_dataset import build_dataset
 from fastai.text import *
 import os
+from models.LSTM import LSTM
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data', type=str, help='path to csv file with data')
+parser.add_argument('--embedding', type=str, help='path to gloVe file with pretrained embeddings')
 args = parser.parse_args()
 
 
@@ -19,7 +21,6 @@ args = parser.parse_args()
 class ModelTrainer():
     def __init__(self):
         #Build dataloaders, vocabulary, and numericalize texts
-
         self.databunch = TextClasDataBunch.from_csv(args.data, bs = 200)
 
 
@@ -36,15 +37,17 @@ class ModelTrainer():
         idx_to_word = self.databunch.vocab.itos
         word_to_idx = build_word_to_idx(idx_to_word)
 
-        import pdb; pdb.set_trace()
-
+        self.model = LSTM(vocab_size = len(idx_to_word), embedding_dim = 300, hidden_size = 20, word_to_idx = word_to_idx, glove_path = args.embedding)
 
         self.train_dataloader = self.databunch.train_dl
         self.valid_dataloader = self.databunch.valid_dl
     def train(self):
-        pass
+        self.model.train()
+        for batch_idx, (data, target) in enumerate(self.train_dataloader):
+            self.model.forward(Variable(data))
 
 
 
 if __name__ == "__main__":
      LSTM = ModelTrainer()
+     LSTM.train()
