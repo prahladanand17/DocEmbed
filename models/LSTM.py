bba1b756c694f5ba5ecb9fa6f06e898ff68499b1
Prahlad <anand39@purdue.edu>
2018-11-10 19:03:05 -0500

    working with embeddings

--- models/LSTM.py
+++ models/LSTM.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import os
+import numpy as np
 from fastai.text import *
 
 
@@ -13,8 +14,8 @@ class LSTM(nn.Module):
         super(LSTM,self).__init__()
         #load pretrained gloVe embeddings
 
-        word_embeds = load_glove(glove_path)
-        embeddings = np.zeros((vocab_size, embedding_size))
+        word_embeds = self.load_glove(glove_path)
+        embeddings = torch.randn((vocab_size, embedding_dim))
         for word in word_embeds.keys():
             index = word_to_idx[word]
             embeddings[index] = word_embeds[word]
@@ -23,9 +24,9 @@ class LSTM(nn.Module):
 
 
         self.embeds = nn.Embedding(vocab_size, embedding_dim)
-        self.doc_LSTM = nn.LSTM(embedding_dim, hidden_size, 1, batch_first = True)
+        self.doc_LSTM = nn.LSTM(embedding_dim, hidden_size, 1, batch_first = True).cuda()
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_size, 50)
+            nn.Linear(hidden_size, 50),
             nn.ReLu(),
             nn.Dropout(p=0.5),
             nn.Linear(50,20)
